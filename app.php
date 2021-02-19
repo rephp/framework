c8ef444ca7a61d6ff95c3032256b10a32509fdfb
sam.shi <32320811@qq.com>
2021-02-19 18:05:57 +0800

    debug

--- app.php
+++ app.php
@@ -45,6 +45,11 @@ class app
      */
     public function run($appPath = '')
     {
+        $whoops = new \Whoops\Run();
+        $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler());
+        $whoops->register();
+        require dirname($appPath) .'/route/route.php';
+        require dirname($appPath) .'/route/api.php';
         //初始化系统运行环境
         $config = $this->container->bind('config', $this->rephpConfig['config']);
         $this->container->bind('appCore', $this->rephpConfig['appCore'])->init($appPath, $config);
