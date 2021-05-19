69d7d239d525466367c97b35c5845eaa54ee0686
sam.shi <32320811@qq.com>
2021-05-19 20:22:20 +0800

    完善model及logic方法调用

--- core/logic.php
+++ core/logic.php
@@ -1,6 +1,8 @@
 <?php
 namespace rephp\core;
 
+use rephp\component\container\container;
+
 /**
  * logic逻辑层抽象类
  * @package rephp\ext
@@ -8,4 +10,32 @@ namespace rephp\core;
 abstract class logic
 {
     use \rephp\traits\publicTrait;
+
+    /**
+     * 调用私有或者不存在的类方法时触发
+     * @param  string  $className  类名字
+     * @param  array   $arguments  参数
+     * @return mixed
+     */
+    public function __call($methodName, $arguments)
+    {
+        $calldClassName = get_called_class();
+        $module = $this->getModuleName();
+
+        $pos = strpos($calldClassName, '\\app\\common\\');
+        if($pos === false){
+            $className = str_replace('\\app\\modules\\' . $module . '\\logic\\', '\\app\\common\\logic\\', $calldClassName);
+        }else{
+            $className = str_replace('\\app\\common\\logic\\', '\\app\\modules\\' . $module . '\\logic\\', $calldClassName);
+        }
+        if(!class_exists($className)){
+            throw new \Exception(404, 'logic not exists:' . $calldClassName);
+        }
+        if(!method_exists($className, $methodName)){
+            throw new \Exception(404, 'Class: '.$calldClassName. ' not exist method:'.$methodName);
+        }
+
+        return container::getContainer()->call($className, $methodName, $arguments);
+    }
+
 }
--- core/model.php
+++ core/model.php
@@ -1,6 +1,8 @@
 <?php
 namespace rephp\core;
 
+use rephp\component\container\container;
+
 /**
  * model抽象类
  * @package rephp\ext
@@ -8,4 +10,32 @@ namespace rephp\core;
 abstract class model
 {
     use \rephp\traits\publicTrait;
+
+    /**
+     * 调用私有或者不存在的类方法时触发
+     * @param  string  $className  类名字
+     * @param  array   $arguments  参数
+     * @return mixed
+     */
+    public function __call($methodName, $arguments)
+    {
+        $calldClassName = get_called_class();
+        $module = $this->getModuleName();
+
+        $pos = strpos($calldClassName, '\\app\\common\\');
+        if($pos === false){
+            $className = str_replace('\\app\\modules\\' . $module . '\\model\\', '\\app\\common\\model\\', $calldClassName);
+        }else{
+            $className = str_replace('\\app\\common\\model\\', '\\app\\modules\\' . $module . '\\model\\', $calldClassName);
+        }
+        if(!class_exists($className)){
+            throw new \Exception(404, 'model not exists:' . $calldClassName);
+        }
+        if(!method_exists($className, $methodName)){
+            throw new \Exception(404, 'Class: '.$calldClassName. ' not exist method:'.$methodName);
+        }
+
+        return container::getContainer()->call($className, $methodName, $arguments);
+    }
+
 }
