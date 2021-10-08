4b014cb1ef1ed986c979303778218de082a6daae
sam.shi <32320811@qq.com>
2021-10-08 16:49:36 +0800

    help

--- src/component/cmd/com/help.php
+++ src/component/cmd/com/help.php
@@ -6,7 +6,22 @@ class help
 
     public function list()
     {
-        echo 'xxxxxx';
+        $output = output::find()->title('用法:', 1, 1)
+                        ->tab()->info('命令 [选项] [参数]', 1, 1)
+                        ->endRow()
+                        ->title('选项:', 1, 1)
+                        ->tab()->success('-h, --help, --h, help, h', 1)->info('显示指定命令的帮助信息。当没有给出命令时，显示')->warning('list')->info('命令的帮助', 1, 1)
+                        ->tab()->success('-q, --quiet', 1)->info('不输出任何消息')
+                        ->tab()->success('-V, v, --version, version', 1)->info('显示此应用程序版本')
+                        ->title('可用命令:', 1, 1);
+        echo $output->get();
+
+        //循环处理内置方法
+        $routeConfigList    = file_exists(ROOT_PATH . 'route/console.php') ? require ROOT_PATH . 'route/console.php' : [];
+        $cliSystemRouteList = require dirname(__DIR__) . '/route/console.php';
+
+        //循环分析外置方法
+
     }
 
 }
