2ba6e514eca296167f7130026d3c3aa3f57e0209
sam.shi <32320811@qq.com>
2022-09-19 12:12:39 +0800

    根据配置项可写日志

--- src/app.php
+++ src/app.php
@@ -28,12 +28,12 @@ class app
      * @var string[]
      */
     private $rephpConfig = [
-        'reponse'     => component\response\response::class,
-        'request'     => component\request\request::class,
-        'coreRoute'   => component\route\com\macawRoute::class,
-        'coreEvent'   => component\event\com\eventV1::class,
-        'coreDebug'   => component\debug\com\debugV1::class,
-        'cmd'         => component\cmd\cmd::class,
+        'reponse'   => component\response\response::class,
+        'request'   => component\request\request::class,
+        'coreRoute' => component\route\com\macawRoute::class,
+        'coreEvent' => component\event\com\eventV1::class,
+        'coreDebug' => component\debug\com\debugV1::class,
+        'cmd'       => component\cmd\cmd::class,
     ];
 
     /**
@@ -43,50 +43,60 @@ class app
      */
     public function run($appPath)
     {
-        try{
+        try {
             //1.预加载资源环境
-            $this->loadEnv(dirname($appPath).'/');
-            $this->loadConfig(dirname($appPath).'/config/');
+            $this->loadEnv(dirname($appPath) . '/');
+            $this->loadConfig(dirname($appPath) . '/config/');
             //2.预先绑定具体组件
             $this->preBindCoreComponent();
             //3.加载核心类 & 运行
             container::getContainer()->bind('appCore', core\appCore::class, [$appPath])->run();
-        }catch (\Exception $e){
-            $isDebug = config('config.debug.is_debug', false);
-            $isDebug && $this->throwInitExcetionWithoutDebug($e);
+        } catch (\Exception $e) {
+            $this->throwInitExcetionWithoutDebugComponent($e);
         }
     }
 
     /**
      * 系统初始化时，来不及加载debug组件时,异常时抛出错误
-     * @param \Exception $e  错误对象
+     * @param \Exception $e 错误对象
      */
-    private function throwInitExcetionWithoutDebug($e)
+    private function throwInitExcetionWithoutDebugComponent($e)
     {
-        $prefix = 'Exception:'."\n";
+        $prefix = 'Exception:' . "\n";
         if ($e instanceof \Error) {
-            $prefix = 'Error:'."\n";
+            $prefix = 'Error:' . "\n";
         }
-        $logContent  = $prefix.'    编号:'.$e->getCode()."\n";
-        $logContent .= '    信息:'.$e->getMessage()."\n";
-        $logContent .= '    文件:'.$e->getFile()."\n";
-        $logContent .= '    行号:'.$e->getLine()."\n";
+        $logContent = $prefix . '    编号:' . $e->getCode() . "\n";
+        $logContent .= '    信息:' . $e->getMessage() . "\n";
+        $logContent .= '    文件:' . $e->getFile() . "\n";
+        $logContent .= '    行号:' . $e->getLine() . "\n";
         if ($e instanceof \Error) {
-            $logContent .= '    追踪:'."\n";
-            $traceList = $e->getTrace();
+            $logContent .= '    追踪:' . "\n";
+            $traceList  = $e->getTrace();
             foreach ($traceList as $traceInfo) {
-                $file  = empty($traceInfo['file']) ? '' : $traceInfo['file'];
+                $file = empty($traceInfo['file']) ? '' : $traceInfo['file'];
                 empty($file) && $file = empty($traceInfo['class']) ? '' : $traceInfo['class'];
-                $logContent .= '        '.$file.'['.$traceInfo['line'].'] => function:'.$traceInfo['function']."\n";
+                $logContent .= '        ' . $file . '[' . $traceInfo['line'] . '] => function:' . $traceInfo['function'] . "\n";
             }
         }
         $logContent .= "--\n";
-        exit($logContent);
+
+        $isDebug = config('config.debug.is_debug', false);
+        if ($isDebug) {
+            exit($logContent);
+        } else {
+            $logFileName = getLogFileName('php');
+            if (empty($logFileName)) {
+                exit('创建PHP日志目录失败');
+            }
+            file_put_contents($logFileName, $logContent, 8);
+        }
+
     }
 
     /**
      * 加载env
-     * @param  string $envPath env路径
+     * @param string $envPath env路径
      * @return boolean
      */
     private function loadEnv($envPath)
@@ -98,7 +108,7 @@ class app
 
     /**
      * 首先需要加载的配置文件
-     * @param string $configPath  配置文件目录
+     * @param string $configPath 配置文件目录
      * @return boolean
      */
     private function loadConfig($configPath)
