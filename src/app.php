27632235469b3c5d1e400a18bf9dea914d652b2f
sam.shi <32320811@qq.com>
2022-11-11 16:56:07 +0800

    改进框架

--- src/app.php
+++ src/app.php
@@ -14,29 +14,7 @@ use rephp\component\debug\debug;
  */
 class app
 {
-    /**
-     * @var string 配置实例类
-     */
-    private $configCom = component\config\com\configV1::class;
-
-    /**
-     * @var string env实例类
-     */
-    private $envCom = component\env\com\envV1::class;
-
-    /**
-     * 初始化默认绑定的对象
-     * @var string[]
-     */
-    private $rephpConfig = [
-        'reponse'   => component\response\response::class,
-        'request'   => component\request\request::class,
-        'coreRoute' => component\route\com\macawRoute::class,
-        'coreEvent' => component\event\com\eventV1::class,
-        'coreDebug' => component\debug\com\debugV1::class,
-        'cmd'       => component\cmd\cmd::class,
-    ];
-
+  
     /**
      * 运行
      * @param string $appPath app路径
@@ -44,96 +22,36 @@ class app
      */
     public function run($appPath)
     {
-        try {
-            //1.预加载资源环境
-            $this->loadEnv(dirname($appPath) . '/');
-            $this->loadConfig(dirname($appPath) . '/config/');
-            //2.预先绑定具体组件
-            $this->preBindCoreComponent();
-            //3.加载核心类 & 运行
-            container::getContainer()->bind('appCore', core\appCore::class, [$appPath])->run();
-        } catch (\Exception $e) {
-            try{
-                container::getContainer()->bind('debug', debug::class);
-            }catch (\Exception $e){
-                $this->throwInitExcetionWithoutDebugComponent($e);
-            }
-        }
+		//定义路径常量
+		$this->definePath($appPath);
+		//加载驱动
+        require_once 'bootstrap/app.php';
+        //初始化时区
+        $this->setTimeZone();
+        //挂载路由
+        container::getContainer()->get('route')->start(ROOT_PATH . 'route/');
+    }
+	
+	/**
+     * 定义路径常量
+     * 系统常量责任人就绪
+	 * @param string $appPath app路径 
+     * @return void
+     */
+    public function definePath($appPath)
+    {
+        define('APP_PATH', $appPath);
+		define('ROOT_PATH', dirname($appPath).'/');
     }
 
     /**
-     * 系统初始化时，来不及加载debug组件时,异常时抛出错误
-     * @param \Exception $e 错误对象
+     * 初始化时区
+     * 时区设定工作责任人就绪
      */
-    private function throwInitExcetionWithoutDebugComponent($e)
+    public function setTimeZone()
     {
-        $prefix = 'Exception:' . "\n";
-        if ($e instanceof \Error) {
-            $prefix = 'Error:' . "\n";
-        }
-        $logContent = $prefix . '    编号:' . $e->getCode() . "\n";
-        $logContent .= '    信息:' . $e->getMessage() . "\n";
-        $logContent .= '    文件:' . $e->getFile() . "\n";
-        $logContent .= '    行号:' . $e->getLine() . "\n";
-        if ($e instanceof \Error) {
-            $logContent .= '    追踪:' . "\n";
-            $traceList  = $e->getTrace();
-            foreach ($traceList as $traceInfo) {
-                $file = empty($traceInfo['file']) ? '' : $traceInfo['file'];
-                empty($file) && $file = empty($traceInfo['class']) ? '' : $traceInfo['class'];
-                $logContent .= '        ' . $file . '[' . $traceInfo['line'] . '] => function:' . $traceInfo['function'] . "\n";
-            }
-        }
-        $logContent .= "--\n";
-
-        $isDebug = config('config.debug.is_debug', false);
-        if ($isDebug) {
-            exit($logContent);
-        } else {
-            $logFileName = getLogFileName('php');
-            if (empty($logFileName)) {
-                exit('创建PHP日志目录失败');
-            }
-            file_put_contents($logFileName, $logContent, 8);
-        }
-
+        $timeZone = config('config.time_zone', 'PRC');
+        date_default_timezone_set($timeZone);
     }
-
-    /**
-     * 加载env
-     * @param string $envPath env路径
-     * @return boolean
-     */
-    private function loadEnv($envPath)
-    {
-        container::getContainer()->bind('coreEnv', $this->envCom);
-        container::getContainer()->bind('env', component\env\env::class, [$envPath]);
-        return true;
-    }
-
-    /**
-     * 首先需要加载的配置文件
-     * @param string $configPath 配置文件目录
-     * @return boolean
-     */
-    private function loadConfig($configPath)
-    {
-        container::getContainer()->bind('coreConfig', $this->configCom);
-        container::getContainer()->bind('config', component\config\config::class, [$configPath]);
-        return true;
-    }
-
-    /**
-     * 预先注册组件
-     * 优先级依赖按照从上往下顺序来加载
-     * @return bool
-     */
-    private function preBindCoreComponent()
-    {
-        foreach ($this->rephpConfig as $bindName => $class) {
-            container::getContainer()->bind($bindName, $class);
-        }
-
-        return true;
-    }
+	
 }
---
+++ src/bootstrap/app.php
@@ -0,0 +1,39 @@
+<?php
+namespace rephp\bootstrap;
+
+use rephp\component\container\container;
+use rephp\component\debug\debug;
+use rephp\component\config\config;
+use rephp\component\env\env;
+use rephp\component\request\request;
+use rephp\component\response\response;
+use rephp\component\cmd\cmd;
+use rephp\component\event\event;
+use rephp\component\route\route;
+
+//必须按顺序加载驱动
+
+//2.env
+container::getContainer()->bind('coreEnv', \rephp\component\env\com\envV1::class);
+container::getContainer()->bind('env', env::class, [ROOT_PATH]);
+//3.config
+container::getContainer()->bind('coreConfig', \rephp\component\config\com\configV1::class);
+container::getContainer()->bind('config', config::class, [ROOT_PATH.'config/']);
+//4.request
+container::getContainer()->bind('request', request::class);
+//1.debug
+container::getContainer()->bind('coreDebug', \rephp\component\debug\com\debugV1::class);
+container::getContainer()->bind('debug', debug::class);
+
+
+
+//5.reponse
+container::getContainer()->bind('response', response::class);
+//6.路由
+container::getContainer()->bind('coreRoute', \rephp\component\route\com\macawRoute::class);
+container::getContainer()->bind('route', route::class);
+//7.事件
+container::getContainer()->bind('coreEvent', \rephp\component\event\com\eventV1::class);
+container::getContainer()->bind('event', event::class);
+//8.cmd
+container::getContainer()->bind('cmd', cmd::class);
